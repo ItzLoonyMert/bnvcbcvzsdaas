63f6578b9b6047bcb893a5af3893077a90b273a1
ItzLoonyMert <46672640+ItzLoonyMert@users.noreply.github.com>
2019-07-18 18:10:03 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,151 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "-";
+
+client.on('message', message => { // Leaked by [ @Out Our server ]
+
+if(!message.channel.guild) return;
+
+if(message.content.startsWith(prefix + 'bc')) {
+
+if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+
+if(!message.member.hasPermission('ADMINISTRATOR')) return
+
+const args = message.content.split(" ").slice(1).join(" ")
+
+const BcList = new Discord.RichEmbed()
+
+.setThumbnail(message.author.avatarURL)
+
+.setAuthor(`محتوى الرساله : ${args}`)
+
+.setDescription(`**برودكاست بـ امبد 📝\nبرودكاست بدون امبد✏ \nلديك دقيقه للأختيار قبل الغاء البرودكاست**`)
+
+if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(BcList).then(msg => {
+
+msg.react('📝')
+
+.then(() => msg.react('✏'))
+
+.then(() =>msg.react('📝'))
+
+
+
+var EmbedBcFilter = (reaction, user) => reaction.emoji.name === '📝' && user.id === message.author.id;
+
+var NormalBcFilter = (reaction, user) => reaction.emoji.name === '✏' && user.id === message.author.id;
+
+
+
+var EmbedBc = msg.createReactionCollector(EmbedBcFilter, { time: 60000 });
+
+var NormalBc = msg.createReactionCollector(NormalBcFilter, { time: 60000 });
+
+
+
+
+
+EmbedBc.on("collect", r => {
+
+
+
+message.channel.send(`:ballot_box_with_check: تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
+
+message.guild.members.forEach(m => {
+
+var EmbedRep = args.replace('<server>' ,message.guild.name).replace('<user>', m).replace('<by>', `${message.author.username}#${message.author.discriminator}`)
+
+var bc = new
+
+Discord.RichEmbed()
+
+.setColor('RANDOM')
+
+.setDescription(EmbedRep)
+
+.setThumbnail(message.author.avatarURL)
+
+m.send({ embed: bc })
+
+msg.delete();
+
+})
+
+})
+
+NormalBc.on("collect", r => {
+
+message.channel.send(`:ballot_box_with_check: تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
+
+message.guild.members.forEach(m => {
+
+var NormalRep = args.replace('<server>' ,message.guild.name).replace('<user>', m).replace('<by>', `${message.author.username}#${message.author.discriminator}`)
+
+m.send(NormalRep);
+
+msg.delete();
+
+})
+
+})
+
+})
+
+}
+
+});
+
+
+
+const devs = ["508574921010577409"]
+
+const adminprefix = "#";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
+    if (message.content === (adminprefix + "Percie")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  } else     
+    if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.sendMessage(`**${argresult}** : Done :>`)
+  return message.reply("**You Can't Change Your Name ,Only After Two Hours :>**");
+  } else
+    if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+        } else     
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.sendMessage(`**:white_check_mark:   ${argresult}**`)
+  }
+    if(message.content === adminprefix + "restart") {
+      if (!devs.includes(message.author.id)) return;
+          message.channel.send(`:warning:️ **Bot restarting by ${message.author.username}**`);
+        console.log("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        console.log(`⚠️ Bot restarting... ⚠️`);
+        console.log("===============================================\n\n");
+        client.destroy();
+        child_process.fork(__dirname + "/bot.js");
+        console.log(`Bot Successfully Restarted`);
+    }
+  
+  });
+
+
+
+client.login(process.env.BOT_TOKEN);
